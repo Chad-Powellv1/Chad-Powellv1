9bec1f65bd5c8c1cf0939232f28a64fead75014e
Chad Powell <chada.powell@outlook.com>
2022-07-22 13:10:17 -0400

    update contact page

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,16 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import AnimatedLetters from '../../services/AnimatedLetters';
 import { Animate } from 'react-simple-animate';
-import { useNavigate } from 'react-router-dom';
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import {
+    EMAIL_SERVICE,
+    EMAIL_TEMPLATE,
+    EMAIL_USER,
+} from '../../services/constants.js';
 import './styles.scss';
 
 export const Contact = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
-    const navigate = useNavigate();
+    const refForm = useRef();
 
-    // Used to navigate to the contact page
-    const handleNavigate = () => {
-        navigate('/contact');
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm(
+                EMAIL_SERVICE,
+                EMAIL_TEMPLATE,
+                refForm.current,
+                EMAIL_USER
+            )
+            .then(
+                (result) => {
+                    alert('Email sent!');
+                    window.location.reload(false)
+                },
+                () => {
+                    alert('Failed to send, please try again.');
+
+                }
+            );
     }
 
     // Used to animate the letters on hover
@@ -45,44 +68,62 @@ export const Contact = () => {
                     <h3 className='text-zone-text'>Let's Chat</h3>
                     <br />
                 </Animate>
-                <div className='text-zone-form'>
-                    <div className='text-zone-form-controlswrapper'>
-                        <div>
-                            <input required name='name' className='inputName' type={'text'} placeholder={'Name'} />
-                        </div>
-                        <div>
-                            <input required name='email' className='inputEmail' type={'text'} placeholder={'Email'} />
-                        </div>
-                        <div>
-                            <input
-                                required
-                                name='description'
-                                className='inputDescription'
-                                type={'text'} placeholder={'Description'}
-                            />
-                        </div>
-                    </div>
-                    <Animate
-                        play
-                        duration={1.5}
-                        delay={1.2}
-                        start={{
-                            opacity: 0,
-                            transform: 'translateY(550px)'
 
-                        }}
-                        end={{
-                            opacity: 1,
-                            transform: 'translateX(0px)'
-                        }}
-                        ease="easeInOut"
-                    >
-                        <div className="contact-me">
-                            <button onClick={handleNavigate}>Send</button>
-                        </div>
-                    </Animate>
+
+                <div className="contact-form">
+                    <form ref={refForm} onSubmit={sendEmail}>
+                        <ul>
+                            <li className="half">
+                                <input placeholder="Name" type="text" name="name" required />
+                            </li>
+                            <li className="half">
+                                <input
+                                    placeholder="Email"
+                                    type="email"
+                                    name="email"
+                                    required
+                                />
+                            </li>
+                            <li>
+                                <input
+                                    placeholder="Subject"
+                                    type="text"
+                                    name="subject"
+                                    required
+                                />
+                            </li>
+                            <li>
+                                <textarea
+                                    placeholder="Message"
+                                    name="message"
+                                    required
+                                ></textarea>
+                            </li>
+                            <Animate
+                                play
+                                duration={1.5}
+                                delay={1.2}
+                                start={{
+                                    opacity: 0,
+                                    transform: 'translateY(550px)'
+
+                                }}
+                                end={{
+                                    opacity: 1,
+                                    transform: 'translateX(0px)'
+                                }}
+                                ease="easeInOut"
+                            >
+                                <li>
+                                    <input type="submit" className="flat-button" value="SEND" />
+                                </li>
+                            </Animate>
+                        </ul>
+                    </form>
                 </div>
             </div>
         </section>
-    )
+    );
 }
+
+
