c9b1eca3dd1e05467b40b66600a49ff461c90545
Chad Powell <chada.powell@outlook.com>
2022-07-27 21:18:32 -0400

    fixed portfolio links

--- src/pages/portfolio/index.jsx
+++ src/pages/portfolio/index.jsx
@@ -15,105 +15,107 @@ import "./styles.scss";
 export const Portfolio = () => {
 
     return (
-        <>
-            <div className='container'>
-                <div
-                    className='row project-container'
-                    data-aos='fade-up'
-                    data-aos-delay='100'
-                >
-                    <div className='col-md-6 project-item filter-app'>
-                        <div className='project-wrap'>
-                            <img src={weather} className='img-fluid' alt='Weather App' />
-                            <div className='project-links'>
-                                <a
-                                    href={weather}
-                                    data-gallery='projectGallery'
-                                    className='project-lightbox'
-                                >
-                                    <i className='bx '>
-                                        <BsPlus />
-                                    </i>
-                                </a>
-                                <a href={WEATHER_APP} title='More Details'>
-                                    <i className='bx'>
-                                        <BsLink45Deg />
-                                    </i>
-                                </a>
+        <main className='main' id='projects'>
+            <section className='project section'>
+                <div className='container'>
+                    <div
+                        className='row project-container'
+                        data-aos='fade-up'
+                        data-aos-delay='100'
+                    >
+                        <div className='col-md-6 project-item filter-app'>
+                            <div className='project-wrap'>
+                                <img src={weather} className='img-fluid' alt='Weather App' />
+                                <div className='project-links'>
+                                    <a
+                                        href={weather}
+                                        data-gallery='projectGallery'
+                                        className='project-lightbox'
+                                    >
+                                        <i className='bx '>
+                                            <BsPlus />
+                                        </i>
+                                    </a>
+                                    <a href={WEATHER_APP} title='More Details'>
+                                        <i className='bx'>
+                                            <BsLink45Deg />
+                                        </i>
+                                    </a>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div className='col-md-6 project-item filter-web'>
-                        <div className='project-wrap'>
-                            <img src={tictactoe} className='img-fluid' alt='Tic Tac Toe' />
-                            <div className='project-links'>
-                                <a
-                                    href={tictactoe}
-                                    data-gallery='projectGallery'
-                                    className='project-lightbox'
-                                >
-                                    <i className='bx'>
-                                        <BsPlus />
-                                    </i>
-                                </a>
-                                <a href={TIC_TAC_TOE} title='More Details'>
-                                    <i className='bx'>
-                                        <BsLink45Deg />
-                                    </i>
-                                </a>
+                        <div className='col-md-6 project-item filter-web'>
+                            <div className='project-wrap'>
+                                <img src={tictactoe} className='img-fluid' alt='Tic Tac Toe' />
+                                <div className='project-links'>
+                                    <a
+                                        href={tictactoe}
+                                        data-gallery='projectGallery'
+                                        className='project-lightbox'
+                                    >
+                                        <i className='bx'>
+                                            <BsPlus />
+                                        </i>
+                                    </a>
+                                    <a href={TIC_TAC_TOE} title='More Details'>
+                                        <i className='bx'>
+                                            <BsLink45Deg />
+                                        </i>
+                                    </a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className='col-md-6 project-item filter-app'>
-                        <div className='project-wrap'>
-                            <img src={todo} className='img-fluid' alt='Todo App' />
-                            <div className='project-links'>
-                                <a
-                                    href={todo}
-                                    data-gallery='projectGallery'
-                                    className='project-lightbox'
-                                >
-                                    <i className='bx'>
-                                        <BsPlus />
-                                    </i>
-                                </a>
-                                <a href={TODO_APP} title='More Details'>
-                                    <i className='bx'>
-                                        <BsLink45Deg />
-                                    </i>
-                                </a>
+                        <div className='col-md-6 project-item filter-app'>
+                            <div className='project-wrap'>
+                                <img src={todo} className='img-fluid' alt='Todo App' />
+                                <div className='project-links'>
+                                    <a
+                                        href={todo}
+                                        data-gallery='projectGallery'
+                                        className='project-lightbox'
+                                    >
+                                        <i className='bx'>
+                                            <BsPlus />
+                                        </i>
+                                    </a>
+                                    <a href={TODO_APP} title='More Details'>
+                                        <i className='bx'>
+                                            <BsLink45Deg />
+                                        </i>
+                                    </a>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div className='col-md-6 project-item filter-card'>
-                        <div className='project-wrap'>
-                            <img
-                                src={restaurant}
-                                className='img-fluid'
-                                alt='Restaurant App'
-                            />
-                            <div className='project-links'>
-                                <a
-                                    href={restaurant}
-                                    data-gallery='projectGallery'
-                                    className='project-lightbox'
-                                >
-                                    <i className='bx'>
-                                        <BsPlus />
-                                    </i>
-                                </a>
-                                <a href={RESTAURANT_APP} title='More Details'>
-                                    <i className='bx'>
-                                        <BsLink45Deg />
-                                    </i>
-                                </a>
+                        <div className='col-md-6 project-item filter-card'>
+                            <div className='project-wrap'>
+                                <img
+                                    src={restaurant}
+                                    className='img-fluid'
+                                    alt='Restaurant App'
+                                />
+                                <div className='project-links'>
+                                    <a
+                                        href={restaurant}
+                                        data-gallery='projectGallery'
+                                        className='project-lightbox'
+                                    >
+                                        <i className='bx'>
+                                            <BsPlus />
+                                        </i>
+                                    </a>
+                                    <a href={RESTAURANT_APP} title='More Details'>
+                                        <i className='bx'>
+                                            <BsLink45Deg />
+                                        </i>
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </>
+            </section>
+        </main>
     );
 };
